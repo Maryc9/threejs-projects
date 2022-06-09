6557f2b0690240e10878554822c19bf1fc37069d
maryc <jmaryy228@gmail.com>
2022-06-09 05:56:32 +0100

    s

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
   
 import './style.css'
-import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js';
+import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.module.js';
+
 
 
 
