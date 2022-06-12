7b3a460542dee4ebb401a363ac868fcc4e0b87bd
iamkepo <christamour.kakpo@gmail.com>
2022-06-12 18:18:57 +0200

    end for test deploy on github.io: gh-pages work

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ const { resolve } = require('path')
 const { defineConfig } = require('vite')
 
 module.exports = defineConfig({
+  base: './',
   assetsInclude: ['**/*.hdr'],
   build: {
     chunkSizeWarningLimit: 1600,
