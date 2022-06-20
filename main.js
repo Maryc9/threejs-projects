0ae8a305b8bdb5d4d8971b5a8bd74bfd1bedf30c
maryc <jmaryy228@gmail.com>
2022-06-20 06:08:08 +0100

    m

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 import './style.css';
 import * as THREE from 'https://unpkg.com/three@0.141.0/build/three.module.js';
-import lava from './lava.jpeg';
-
-
-
-
-
 
 
 
@@ -24,25 +18,10 @@ camera.position.setZ(25);
 
 renderer.render(scene, camera);
 const sphere = new THREE.Mesh(
-  new THREE.SphereGeometry(10, 50, 50),
-  new THREE.MeshNormalMaterial({
-  
-    // color: 0xff758f,
-    shininess: 100,
-    wireframe: true
-  })
-);
-sphere.position.x = 2.5;
-scene.add(sphere);
-
-const sphere2 = new THREE.Mesh(
-  new THREE.SphereGeometry(10, 50, 50),
+  new THREE.SphereGeometry(4, 50, 50),
   new THREE.MeshNormalMaterial()
 )
-sphere2.position.x = 1;
-scene.add(sphere2);
-
-
+scene.add(sphere);
 // light
 const point = new THREE.PointLight(0x11ff7b, 10 )
 const ambient =  new THREE.AmbientLight(0x11ff7b, 4)
