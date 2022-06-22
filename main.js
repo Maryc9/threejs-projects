9aa2098b47e2d1fa8266f3c8c8dcf3285fa5d3ab
maryc <jmaryy228@gmail.com>
2022-06-22 10:05:36 +0100

    m

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 import './style.css';
 import * as THREE from 'https://unpkg.com/three@0.141.0/build/three.module.js';
+import { FlyControls } from './FlyControls.js';
+import space from './c132.jpeg'
+import star from './star.jpg'
 
 
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xf8f9fa)
+scene.background = new THREE.Color(0x000000)
 const camera = new THREE.PerspectiveCamera(75, innerWidth/innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg2')
@@ -17,14 +20,51 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(25);
 
 renderer.render(scene, camera);
+
+let space = new THREE.TextureLoader().load('./c132.jpeg');
+let star = new THREE.TextureLoader().load('./star.jpg');
+
+
+
+const flyControl = new FlyControls(camera, renderer.domElement);
+flyControl.movementSpeed = 1;
+flyControl.rollSpeed = 0.05;
+flyControl.autoForward = false;
+flyControl.dragToLook = false;
+
+
 const sphere = new THREE.Mesh(
-  new THREE.SphereGeometry(4, 50, 50),
-  new THREE.MeshNormalMaterial()
+  new THREE.SphereGeometry(10, 50, 50),
+  new THREE.MeshPhongMaterial({
+   map: space,
+   shininess: 100
+  })
 )
 scene.add(sphere);
+
+
+function addStar(){
+  const geo = new THREE.SphereGeometry(0.07, 24, 24);
+  const mat = new THREE.MeshStandardMaterial({
+    color: 0xaaaaaa,
+    map: star
+  })
+  const starry = new THREE.Mesh(geo, mat);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+starry.position.set(x, y, z);
+scene.add(starry);
+}
+
+Array(6000).fill().forEach(addStar);
+
+
+
+
+
 // light
-const point = new THREE.PointLight(0x11ff7b, 10 )
-const ambient =  new THREE.AmbientLight(0x11ff7b, 4)
+const point = new THREE.PointLight(0xffffff,  )
+const ambient =  new THREE.AmbientLight(0xffffff, )
 
 scene.add(ambient, point)
 
@@ -32,9 +72,7 @@ scene.add(ambient, point)
 
 
 
-// scroll
-// torus.position.z = 1;
-// torus.position.setX(-0)
+
 
 
 
@@ -43,9 +81,8 @@ scene.add(ambient, point)
 function animate(){
   requestAnimationFrame(animate)
   renderer.render(scene, camera);
+  flyControl.update(0.05);
 
- sphere.rotation.y += 0.002;
- 
 
 
 }
